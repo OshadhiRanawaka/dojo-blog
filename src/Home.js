3a3aafe2b46e884587730211b152e764572d7f1f
Oshadhi <oshadhiranawaka@gmail.com>
2026-04-19 19:08:53 +0530

    Refactor Home component to improve error handling and fix state variable naming

--- src/Home.js
+++ src/Home.js
@@ -3,25 +3,34 @@ import { useState, useEffect } from "react";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-  const [isPending, setPending] = useState(true);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
 
   useEffect(() => {
     setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-        setPending(false);
-      });
+        fetch("http://localhost:8000/blogs")
+          .then((res) => {
+            if (!res.ok) {
+              throw Error("could not fetch the data for that resource");
+            }
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+          .catch((err) => {
+            setError(err.message);
+            setIsPending(false);
+          })
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
