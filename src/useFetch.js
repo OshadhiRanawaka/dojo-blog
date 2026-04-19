a0406cd40b46ff4cedb46853583bfe366cd55a9c
Oshadhi <oshadhiranawaka@gmail.com>
2026-04-19 19:47:27 +0530

    Refactor useFetch hook to improve error handling and add abort controller for fetch requests

--- src/useFetch.js
+++ src/useFetch.js
@@ -2,15 +2,16 @@ import { useState, useEffect } from "react";
 
 
 const useFetch = (url) => {
-
     const [data, setData] = useState(null);
-  const [isPending, setIsPending] = useState(true);
+    const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
 
 
-    useEffect(() => {
+  useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-        fetch(url)
+        fetch(url , { signal: abortCont.signal })
           .then((res) => {
             if (!res.ok) {
               throw Error("could not fetch the data for that resource");
@@ -23,11 +24,17 @@ const useFetch = (url) => {
           setError(null);
         })
           .catch((err) => {
-            setError(err.message);
-            setIsPending(false);
+            if (err.name === "AbortError") {
+              console.log("fetch aborted");
+            } else {
+              setIsPending(false);
+              setError(err.message);
+            }
           })
     }, 1000);
-    }, []);
+
+      return () => abortCont.abort();
+    }, [url]);
 
     return { data, isPending, error };
 }
