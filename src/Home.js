e2ca59ea5dfddf9910ac278887004ddf980e41ec
Oshadhi <oshadhiranawaka@gmail.com>
2026-04-19 18:58:35 +0530

    Remove handleDelete function and button from BlogList component; update Home component to reflect changes

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,29 @@
 import BlogList from "./BlogList";
-import { useState , useEffect} from "react";
+import { useState, useEffect } from "react";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setPending] = useState(true);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-  };
+
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+      .then((res) => {
         return res.json();
       })
-      .then(data => {
+      .then((data) => {
         console.log(data);
         setBlogs(data);
+        setPending(false);
       });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
